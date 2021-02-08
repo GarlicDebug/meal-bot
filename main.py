3768316bc200de8d440f867038e140847eb42a1a
reise <reise@andrews.edu>
2021-02-08 00:02:06 -0500

    Initial Commit. Bot has basic functionality and the menu scraping is working.

---
+++ main.py
@@ -0,0 +1,48 @@
+import discord
+from mealFinder import MealFinder
+from discord.ext import commands, tasks
+
+# Discord API token is stored in unversioned .env file for safety
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+token = os.environ.get("discord-token")
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+    MealFinder.getmeal("dinner", numEntries=10)
+
+
+@client.event
+async def on_member_join(self, member):
+    guild = member.guild
+    if guild.system_channel is not None:
+        to_send = 'Welcome {0.mention} to {1.name}!'.format(member, guild)
+        await guild.system_channel.send(to_send)
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$beep'):
+        await message.channel.send('boop!')
+
+
+@client.event
+async def on_message(self, message):
+    # we do not want the bot to reply to itself
+    if message.author.id == self.user.id:
+        return
+
+    if message.content.startswith('!hello'):
+        await message.reply('Hello!', mention_author=True)
+
+
+client.run(token)
---
+++ mealFinder.py
@@ -0,0 +1,27 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+class MealFinder:
+    URL = 'https://andrews-university.cafebonappetit.com/cafe/terrace-cafe/'
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    '''@mealname is a string for the name of the meal'''
+
+    @staticmethod
+    def getmeal(mealname, numEntries):
+        meal = MealFinder.soup.find(id=mealname)
+
+        meal_items = meal.find_all(class_="h4 site-panel__daypart-item-title")
+
+        menuString = "Today's " + mealname + " options are: \n"
+
+        for meal_item in meal_items[:numEntries]:
+            menuString += meal_item.text.strip() + "\n"
+
+        #print(menuString)
+
+        return menuString
+ #TODO: Clean out "Upon request"
+ #TODO: indicate that multiples are vegan
